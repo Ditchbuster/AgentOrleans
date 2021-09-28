e26e6ef9fe07c189066248db477032d31987dd01
Ditchbuster <trioman008@hotmail.com>
2021-09-27 21:16:26 -0500

    added test clients, resources to seed data

--- IdServer/SeedData.cs
+++ IdServer/SeedData.cs
@@ -5,8 +5,14 @@
 using System;
 using System.Linq;
 using System.Security.Claims;
+using System.Collections.Generic;
+using System.Reflection;
 using IdentityModel;
 using IdServer.Data;
+using Duende.IdentityServer.EntityFramework.DbContexts;
+using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,13 +25,27 @@ namespace IdServer
         public static void EnsureSeedData(string connectionString)
         {
             var services = new ServiceCollection();
-            services.AddLogging();
-            services.AddDbContext<ApplicationDbContext>(options =>
-               options.UseSqlite(connectionString, o => o.MigrationsAssembly(typeof(Startup).Assembly.FullName)));
 
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseSqlite(connectionString,
+                        o => o.MigrationsAssembly(migrationsAssembly));
+                });
             services.AddIdentity<ApplicationUser, ApplicationRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            var identityServerBuilder = services.AddIdentityServer(options => options.KeyManagement.Enabled = true);
+
+            identityServerBuilder.AddOperationalStore(options =>
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlite(connectionString, sqlOptions => sqlOptions.MigrationsAssembly(migrationsAssembly)))
+                .AddConfigurationStore(options =>
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlite(connectionString, sqlOptions => sqlOptions.MigrationsAssembly(migrationsAssembly)));
+
+            identityServerBuilder.AddAspNetIdentity<ApplicationUser>();
 
             using (var serviceProvider = services.BuildServiceProvider())
             {
@@ -99,8 +119,108 @@ namespace IdServer
                     {
                         Log.Debug("bob already exists");
                     }
+
+                    Log.Debug("Seeding configuration data...");
+                    scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+                    scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>().Database.Migrate();
+
+                    var ConfigContext = scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+
+                    if (!ConfigContext.Clients.Any())
+                    {
+                        foreach (var client in Clients.Get())
+                        {
+                            ConfigContext.Clients.Add(client.ToEntity());
+                        }
+                        ConfigContext.SaveChanges();
+                    }
+
+
+
                 }
             }
+        }
+    }
+
+    internal class Clients //TODO: move to config file
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "ConsoleClient",
+                    ClientName = "Console Client",
+                    Description = "Example client application using client credentials",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = new List<Secret> {new Secret("SuperSecretPassword".Sha256())}, // change me!
+                    AllowedScopes = new List<string> {"gameapi1.read, gameapi1.write"}
+                }/*,
+                new Client
+                {
+                    ClientId = "oidcClient",
+                    ClientName = "Example Client Application",
+                    ClientSecrets = new List<Secret> {new Secret("SuperSecretPassword".Sha256())}, // change me!
+                    
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = new List<string> {"https://localhost:5002/signin-oidc"},
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "role",
+                        "api1.read"
+                    },
+
+                    RequirePkce = true,
+                    AllowPlainTextPkce = false
+                } */
+            };
+        }
+    }
+
+    internal class Resources
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource
+                {
+                    Name = "role",
+                    UserClaims = new List<string> {"role"}
+                }
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new[]
+            {
+                new ApiResource
+                {
+                    Name = "gameapi1",
+                    DisplayName = "GAME API V1",
+                    Description = "Allow the application to access the GAME API V1 on your behalf",
+                    Scopes = new List<string> {"gameapi1.read", "gameapi1.write"},
+                    ApiSecrets = new List<Secret> {new Secret("ScopeSecret".Sha256())}, // TODO: change me - API secret!
+                    UserClaims = new List<string> {"role"}
+                }
+            };
+        }
+
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new[]
+            {
+                new ApiScope("gameapi1.read", "Read Access to GAME API V1"),
+                new ApiScope("gameapi1.write", "Write Access to GAME API V1")
+            };
         }
     }
 }
