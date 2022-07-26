d9ce4d5cc90e6c9c10a49beee93993cbd8b55d3b
Ditchbuster <trioman008@hotmail.com>
2022-07-25 23:29:08 -0600

    add agentgrain interface

---
+++ AgentContract/IAgentGrain.cs
@@ -0,0 +1,17 @@
+using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentContract
+{
+    /// <summary>
+    /// The grain represents the user inside the silo, coordinates with clients outside the silo, receive and dispatch actions for the user
+    /// </summary>
+
+    public interface IAgentGrain : IGrainWithGuidKey
+    {
+        Task<UserData> GetAgentData();
+    }
+}
